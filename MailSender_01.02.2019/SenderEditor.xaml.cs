1ea160dfb835ebdc102a98cc294b4c7901cf4ce6
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2020-02-01 23:52:55 +0300

    Начало сборки SenderEditor

---
+++ MailSender_01.02.2019/SenderEditor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MailSender_01._02._2019
+{
+    /// <summary>
+    /// Логика взаимодействия для SenderEditor.xaml
+    /// </summary>
+    public partial class SenderEditor : Window
+    {
+        public SenderEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
