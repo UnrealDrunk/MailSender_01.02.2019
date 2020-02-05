5ec72b32be376741333a7cd1f00c2b18df4a2833
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2020-02-06 00:50:14 +0300

    Из тестового файла очищен весь мусор Начата работа над ViewModel тестового файла, дочерним классом от абстрактного

---
+++ Test_1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSender.lib.MVVM;
+
+namespace Test_1.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Наше новое окно!!!";
+
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (Equals(_Title, value)) return;
+                _Title = value;
+                //OnPropertyChanged("Title");
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
