3f06cbbe537515e68836c662f4a6acd8dc9fae54
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2020-02-06 00:32:58 +0300

    Готова модель представления

--- MailSender.lib/MVVM/ViewModel.cs
+++ MailSender.lib/MVVM/ViewModel.cs
@@ -17,5 +17,13 @@ namespace MailSender.lib.MVVM
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
         }
 
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
     }
 }
