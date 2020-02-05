1d6a0482c481262fdf6d04446ecbc4e82901f916
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2020-02-06 00:27:42 +0300

    Минимум, с которыым можно жить в MVVM ViewModel

---
+++ MailSender.lib/MVVM/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace MailSender.lib.MVVM
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+    }
+}
