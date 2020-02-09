939949b67f0383121b2b630af4a56fc319ac01bc
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2020-02-09 21:53:42 +0300

    Простейшая комманда MVVMLight лямбда-команда

--- MailSender.lib/MVVM/LambdaCommand.cs
+++ MailSender.lib/MVVM/LambdaCommand.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MailSender.lib.MVVM
 {
-    class LambdaCommand
+    public class LambdaCommand : ICommand
     {
+        public event EventHandler CanExecuteChanged;
 
+        private Action<object> _CommandAction;
+        private Func<object, bool> _CanExecute;
+
+
+        public LambdaCommand(Action<object> CommandAction, Func<object, bool> CanExecute = null)
+        {
+            _CommandAction = CommandAction;
+            _CanExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+       
+
+        public void Execute(object parameter) => _CommandAction(parameter);
+       
     }
 }
