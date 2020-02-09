5a8ba9abd01eb891f42827827b5e305682e284d6
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2020-02-09 16:02:21 +0300

    Подключение базововой инфраструктуры MVVMLight

---
+++ MailSender_01.02.2019/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+using GalaSoft.MvvmLight;
+
+namespace MailSender_01._02._2019.ViewModel
+{
+ 
+    public class MainViewModel : ViewModelBase
+    {
+      
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
+    }
+}
---
+++ MailSender_01.02.2019/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,43 @@
+
+
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using CommonServiceLocator;
+namespace MailSender_01._02._2019.ViewModel
+{
+ 
+    public class ViewModelLocator
+    {
+        
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+        
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
