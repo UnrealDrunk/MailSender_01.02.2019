60c89c0d006c1bfd7b1640d28542584b6f73cad7
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2020-02-19 22:39:05 +0300

    переработка  сервиса хранилища данных получателей писем в памяти

--- MailSender.lib/Services/InMemory/RecipientsStoreInMemory.cs
+++ MailSender.lib/Services/InMemory/RecipientsStoreInMemory.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +8,15 @@ using MailSender.lib.Services.Iterfaces2;
 
 namespace MailSender.lib.Services
 {
-    public class RecipientsStoreInMemory : IRecipientsStore
+
+
+    public class RecipientsStoreInMemory : DataStoreInMemeory<Recipients>, IRecipientsStore
     {
 
-        public IEnumerable<Recipients> GetAll() => TestData.Recipients;
+        public RecipientsStoreInMemory(): base(TestData.Recipients) { }
 
 
-        public Recipients GetById(int id) => TestData.Recipients.FirstOrDefault(r => r.Id == id);
-
-
-
-        public int Create(Recipients Recipient)
-        {
-            if (TestData.Recipients.Contains(Recipient)) return Recipient.Id;
-            Recipient.Id = TestData.Recipients.Count == 0 ? 1 : TestData.Recipients.Max(r => r.Id) + 1;
-            TestData.Recipients.Add(Recipient);
-            return Recipient.Id;
-        }
-
-        public void Edit(int id, Recipients recipient)
+        public override void Edit(int id, Recipients recipient)
         {
             var db_recipient = GetById(id);
             if (db_recipient is null) return;
@@ -39,25 +28,7 @@ namespace MailSender.lib.Services
         }
 
 
-        public Recipients Remove(int id)
-        {
-            var db_recipient = GetById(id);
-            if(db_recipient != null)
-            {
-                TestData.Recipients.Remove(db_recipient);
-            }
-            return db_recipient;
-        }
 
-        public void SaveChanges()
-        {
-            System.Diagnostics.Debug.WriteLine("Сохранение изменений в хранилище получателей писем");
-            //поскольку это хранилище данных в памяти, то здесь ничего не деалем
-        }
 
     }
-
-
-
-
 }
