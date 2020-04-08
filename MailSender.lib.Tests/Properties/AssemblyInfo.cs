d3f57a5a4760e221cd4b614c31dcb75aae744418
vs.kozh@list.ru <vs.kozh@list.ru>
2020-04-08 15:47:07 +0300

    Первый модульный тест

---
+++ MailSender.lib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MailSender.lib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MailSender.lib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dabfe5f4-1e9c-4920-8040-deb5d8d3d387")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MailSender.lib.Tests/Service/TestEncoderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MailSender.lib.Service;
+
+
+namespace MailSender.lib.Tests.Service
+{
+    [TestClass]
+    public class TestEncoderTests
+    {
+        [TestMethod]
+        public void Encode_ABC_to_BCD_with_key_1()
+        {
+            //A-A-A = Arrange - Act - Assert
+
+            const string str = "ABC";
+            const int key = 1;
+            const string expected_str = "BCD";
+
+
+            var actual_str = TextEncoder.Encode(str, key);
+
+            Assert.AreEqual(expected_str, actual_str);
+        }
+
+    }
+}
