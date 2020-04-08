1bd6e72e1adfdd7d002a9f0e83bf13427cb65167
vs.kozh@list.ru <vs.kozh@list.ru>
2020-04-08 17:17:13 +0300

    жизненнный цикл тетсов

--- MailSender.lib/Services/InMemory/DataStoreInMemeory.cs
+++ MailSender.lib/Services/InMemory/DataStoreInMemeory.cs
@@ -15,6 +15,7 @@ namespace MailSender.lib.Services
 
         public int Create(T item)
         {
+            if (item is null) throw new ArgumentNullException(nameof(item));
             if (_Items.Contains(item)) return item.Id;
             item.Id = _Items.Count == 0 ? 1 : _Items.Max(r => r.Id) + 1;
             _Items.Add(item);
