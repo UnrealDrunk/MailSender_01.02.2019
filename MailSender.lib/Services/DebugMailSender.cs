25f4fa4c1fb53cac43d4be9afb37c928b2ecafab
vs.kozh@list.ru <vs.kozh@list.ru>
2020-04-19 16:43:56 +0300

    Рефакторинг класса отправки почты

---
+++ MailSender.lib/Services/DebugMailSender.cs
@@ -0,0 +1,30 @@
+﻿using MailSender.lib.Entities;
+using System.Diagnostics;
+
+namespace MailSender.lib.Services
+{
+    public class DebugMailSender
+    {
+        private readonly Server _Server;
+
+        public DebugMailSender(Server Server)
+        {
+            _Server = Server;
+  
+        }
+
+        public void Send( Mail Mail, Sender From, Recipients To)
+        {
+            Debug.WriteLine("Отправка почты от {0} к {1} через {2}:{3}[{4}]\r\n{5}:{6}",
+                From.Adress, To.Adress, _Server.Adress, _Server.Port,
+                _Server.UseSSL ? "SSL" : "no SSL", Mail.Subject, Mail.Body);
+        }
+
+
+
+    }
+
+
+
+
+}
--- MailSender.lib/Services/MailSender.cs
+++ MailSender.lib/Services/MailSender.cs
@@ -6,66 +6,30 @@ using System.Threading.Tasks;
 using System.Net.Mail;
 using System.Net;
 using System.Security;
-using System.Diagnostics;
+using MailSender.lib.Entities;
 
 namespace MailSender.lib.Services
 {
     public class MailSender
     {
-        private string serverAdress;
-        private readonly int port;
-        private bool useSsl;
-        private string login;
-        private string password;
+        private readonly Server _Server;
 
+        public MailSender(Server Server) => _Server = Server;
 
-        public MailSender(string serverAdress, int port, bool useSsl, string login, string password)
+        public void Send(Mail Mail, Sender From, Recipients To)
         {
-            this.serverAdress = serverAdress;
-            this.port = port;
-            this.useSsl = useSsl;
-            this.login = login;
-            this.password = password;
-        }
-
-        public void Send(string subject, string body, string from, string to)
-        {
-            using (var message = new MailMessage(from, to))
+            using (var message = new MailMessage(new MailAddress(From.Adress, From.Name), 
+                new MailAddress(To.Adress, To.Name)))
             {
-                message.Subject = subject;
-                message.Body = body;
+                message.Subject = Mail.Subject;
+                message.Body = Mail.Body;
 
-                var _login = new NetworkCredential(login, password);
-                using (var client = new SmtpClient(serverAdress, port) { EnableSsl = useSsl, Credentials = _login })
+                var _login = new NetworkCredential(_Server.Login, _Server.Password);
+                using (var client = new SmtpClient(_Server.Adress, _Server.Port) { EnableSsl = _Server.UseSSL, Credentials = _login })
                     client.Send(message);
                     
                
             }
-        }
-    }
-
-    public class DebugMailSender
-    {
-        private string serverAdress;
-        private readonly int port;
-        private bool useSsl;
-        private string login;
-        private string password;
-
-
-        public DebugMailSender(string serverAdress, int port, bool useSsl, string login, string password)
-        {
-            this.serverAdress = serverAdress;
-            this.port = port;
-            this.useSsl = useSsl;
-            this.login = login;
-            this.password = password;
-        }
-
-        public void Send(string subject, string body, string from, string to)
-        {
-            Debug.WriteLine("Отправка почты от {0} к {1} через {2}:{3}[{4}]\r\n{5}:{6}", from, to, serverAdress, port,
-                useSsl ? "SSL" : "no SSL", subject, body);
         }
     }
 
