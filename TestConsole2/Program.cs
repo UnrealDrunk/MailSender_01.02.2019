88e61c3c79e37582268ed6902f87a043b311b78c
vs.kozh@list.ru <vs.kozh@list.ru>
2020-05-03 15:09:11 +0300

    Parallel.for

--- TestConsole2/Program.cs
+++ TestConsole2/Program.cs
@@ -20,7 +20,7 @@ namespace TestConsole2
 
 
             //Второй способ реализации асинхронных операций
-            Action<string> printer = str => Console.WriteLine("Печать сообщения из потока{0}:{1}", Thread.CurrentThread.ManagedThreadId ,str);
+            Action<string> printer = str => Console.WriteLine("Печать сообщения из потока{0}:{1}", Thread.CurrentThread.ManagedThreadId, str);
             //printer("Message 1");
             //printer.Invoke("Messsage 2");
 
@@ -56,8 +56,19 @@ namespace TestConsole2
 
             //var messages = Enumerable.Range(1, 100).Select(i => $"Message {i:000}.").ToArray();
 
+            //Parallel.For(0, 100,
+            //    new ParallelOptions { MaxDegreeOfParallelism = 3},
+            //    i => ParallelInvokeMethod($"Message {i}"));
 
+            var for_result = Parallel.For(0, 100,
+             new ParallelOptions { MaxDegreeOfParallelism = 3 },
+             (i, state) =>
+             {
+                 if (i > 15) state.Break();
+                 ParallelInvokeMethod($"Message {i}");
+             });
 
+            Console.WriteLine("Выполнилось {0} итераций", for_result.LowestBreakIteration);
             Console.ReadLine();
             Console.WriteLine("Приложение должно быть закрыто");
 
@@ -69,6 +80,14 @@ namespace TestConsole2
             Thread.Sleep(250);
             Console.WriteLine("ThrID:{0} - finished", Thread.CurrentThread.ManagedThreadId);
         }
+
+        private static void ParallelInvokeMethod(string msg)
+        {
+            Console.WriteLine("ThrID: {0} - started: {1}", Thread.CurrentThread.ManagedThreadId, msg);
+            Thread.Sleep(250);
+            Console.WriteLine("ThrID:{0} - finished: {1}", Thread.CurrentThread.ManagedThreadId, msg);
+        }
+
 
     }
 }
